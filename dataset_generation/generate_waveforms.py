2691bf6e1c4428d3b3f63d8a4869063a735516da
Vincent Kenbeek <vkenbeek@gmail.com>
2024-02-25 09:39:47 +0100

    Added possibility to add optional waveforms

--- dataset_generation/generate_waveforms.py
+++ dataset_generation/generate_waveforms.py
@@ -5,7 +5,8 @@ import re
 
 import threading
 import multiprocessing
-
+import random
+import math
 import json
 from concurrent.futures import ThreadPoolExecutor
 from multiprocessing import Pool
@@ -24,16 +25,20 @@ def callback(result):
     success += result
     
         
-def setup_and_run_simulation(input):
+def generate_waveform(input):
     directory = input
 
     dump_file = os.path.join(directory, "dump.vcd")
     timer_file = os.path.join(directory, "timer.json")
+    timer_file1 = os.path.join(directory, "timer1.json")
+    timer_file2 = os.path.join(directory, "timer2.json")
+    timer_file3 = os.path.join(directory, "timer3.json")
     diag_file = os.path.join(directory, "timingdiagram.png")
     
     try:
         subprocess.run(["python3", "-m", "vcd2wavedrom.vcd2wavedrom", "-i", dump_file, "-o", timer_file], shell=True, check=True, stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
         data = {}
+        additional_waveforms = False
         has_large_data_fields = False
         # open timer.json
         with open(timer_file, 'r') as f:
@@ -46,6 +51,7 @@ def setup_and_run_simulation(input):
                 if any(len(d) > 4 for d in signal["data"]):
                     has_large_data_fields = True
                     break
+
         # if has_large_data_fields is True, change data["config"]["hscale"] to 3
         if has_large_data_fields:
             try:
@@ -55,7 +61,24 @@ def setup_and_run_simulation(input):
                     data["config"] = {"hscale": 3}
                 except:
                     pass
-            
+        
+        if len(data["signal"]) > 6:
+            try:
+                subset = data["signal"][2:] # first two signals are often clock and reset, try to keep those where they are
+                optional_d1 = random.shuffle(subset)
+                optional_d2 = random.shuffle(subset)
+                optional_d3 = random.shuffle(subset)
+                # write the shuffled data to timer1.json, timer2.json, and timer3.json
+                with open(timer_file1, 'w') as f:
+                    json.dump({"signal": data["signal"][:2] + optional_d1, "config": data["config"]}, f, indent=4)
+                with open(timer_file2, 'w') as f:
+                    json.dump({"signal": data["signal"][:2] + optional_d2, "config": data["config"]}, f, indent=4)
+                with open(timer_file3, 'w') as f:
+                    json.dump({"signal": data["signal"][:2] + optional_d3, "config": data["config"]}, f, indent=4)
+                additional_waveforms = True
+            except:
+                pass
+
         # write the new data to timer.json
         with open(timer_file, 'w') as f:
             json.dump(data, f, indent=4)
@@ -64,6 +87,13 @@ def setup_and_run_simulation(input):
     # return 0
     try:
         subprocess.run(["wavedrom-cli", "-i", timer_file, "-p", diag_file], shell=True, stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL, check=True)
+        if additional_waveforms:
+            try:
+                subprocess.run(["wavedrom-cli", "-i", timer_file1, "-p", diag_file.replace("timingdiagram", "optional_timingdiagram1")], shell=True, stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL, check=True)
+                subprocess.run(["wavedrom-cli", "-i", timer_file2, "-p", diag_file.replace("timingdiagram", "optional_timingdiagram2")], shell=True, stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL, check=True)
+                subprocess.run(["wavedrom-cli", "-i", timer_file3, "-p", diag_file.replace("timingdiagram", "optional_timingdiagram3")], shell=True, stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL, check=True)
+            except:
+                pass
     except:
         return 0
     return 1
@@ -87,7 +117,7 @@ def create_waveforms():
     print("Set up waveform tasks")
     total_tasks = len(create_sim_file_tasks)
     for task in create_sim_file_tasks:
-        pool.apply_async(setup_and_run_simulation, args=(task,), callback=callback)
+        pool.apply_async(generate_waveform, args=(task,), callback=callback)
     while total_tasks > completed:
         print("Number of tasks left: {}".format(total_tasks - completed))
         time.sleep(30)
